d39f4e23acba4ecfcd2d64daabe65017ccb7b620
Lucian Neag <lucian@amazeelabs.com>
2015-02-10 16:21:22 +0100

    FOR-1521 add event support based on link href attribute

--- intercomio.js
+++ intercomio.js
@@ -4,21 +4,47 @@
   /**
    * Trigger Intercom.io events on click for links with 'data-intercom-event' attribute
    */
-  Drupal.behaviors.intercomioLinkClickEvents = {
+  Drupal.behaviors.intercomioDataLinkClickEvents = {
     attach: function(context, settings) {
-      $('a[data-intercom-event]').each(function(){
-        var $this = $(this);
-        var event_name = $this.data('intercom-event');
-        $this.bind('click', function(){
-          if (typeof Intercom != 'undefined') {
-            try {
-              Intercom('trackEvent', event_name);
+      $('a[data-intercom-event]', context)
+        .once('intercomio-click')
+        .each(function(){
+          var $this = $(this);
+          var event_name = $this.data('intercom-event');
+          $this.bind('click', function(){
+            if (typeof Intercom != 'undefined') {
+              try {
+                Intercom('trackEvent', event_name);
+              }
+              catch(e) {}
             }
-            catch(e) {}
-          }
-        })
-      });
+          })
+        });
     }
   };
+
+  /**
+   * Trigger Intercom.io events on click for links with href specified in settings
+   */
+  Drupal.behaviors.intercomioHrefLinkClickEvents = {
+    attach: function(context, settings) {
+      if (typeof settings.intercomio != 'undefined' && typeof settings.intercomio.link_events != 'undefined') {
+        for(i in settings.intercomio.link_events) {
+          if (settings.intercomio.link_events.hasOwnProperty(i)) {
+            var event_name = settings.intercomio.link_events[i].event_name;
+            var link_href = settings.intercomio.link_events[i].url;
+            $('a[href="' + link_href + '"]')
+              .once('intercomio-click')
+              .each(function(){
+                $(this).bind('click', function() {
+                  Intercom('trackEvent', event_name);
+                })
+              });
+          }
+        }
+      }
+    }
+  }
+
 
 })(jQuery);
